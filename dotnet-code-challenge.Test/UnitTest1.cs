ad2dad964ec98330463fba8d25ca00918122054d
shumanpaul <shumanpaul@users.noreply.github.com>
2019-08-26 05:04:06 +1000

    Initial Test Data

--- dotnet-code-challenge.Test/UnitTest1.cs
+++ dotnet-code-challenge.Test/UnitTest1.cs
@@ -5,8 +5,65 @@ namespace dotnet_code_challenge.Test
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// Invalid path or missing file
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void ListHorses_FileNotFoundException()
+        {
+
+        }
+
+        /// <summary>
+        /// File exist but empty
+        /// </summary>
+        [Fact]
+        public void ListHorses_EmptyFile()
+        {
+
+        }
+
+        /// <summary>
+        /// Empty string passed as file path
+        /// </summary>
+        [Fact]
+        public void ListHorses_BlankFilePath()
+        {
+
+        }
+
+        /// <summary>
+        /// JSON Input file corrupt
+        /// </summary>
+        [Fact]
+        public void ListHorses_InCorrectFromatJSONFile()
+        {
+
+        }
+
+        /// <summary>
+        /// XML Input file corrupt
+        /// </summary>
+        [Fact]
+        public void ListHorses_InCorrectFromatXMLFile()
+        {
+
+        }
+
+        /// <summary>
+        /// Input file doesnt contain any horses or required data
+        /// </summary>
+        [Fact]
+        public void ListHorses_NoHorsesInInputFile()
+        {
+
+        }
+
+        /// <summary>
+        /// Valid Case 1
+        /// </summary>
+        [Fact]
+        public void ListHorses_ValidCase1()
         {
 
         }
